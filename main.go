b0a4f1cb86c26b8e1a8862f651ab384ef644d680
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2023-11-21 13:03:33 -0300

    using struct

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+//
+//func main() {
+//	println("Hello World")
+//}
+
+type Car struct {
+	Model string
+	Color string
+}
+
 func main() {
-	println("Hello World")
+	car := Car{
+		Model: "Lamborghini",
+		Color: "yellow",
+	}
+	println("Model: " + car.Model + " Color: " + car.Color)
 }
